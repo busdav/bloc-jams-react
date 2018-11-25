f7c912eb1f817ddb723169d3a14e21bec4bfa297
David Buser <busdav@outlook.com>
2018-11-25 10:48:37 +0000

    completed audio playback

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,7 +10,37 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
+    }
+
+    this.audioElement = document.createElement("audio");
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false })
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
     }
   }
 
@@ -32,14 +62,9 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>  
           <tbody>
-            <tr>
-              <th>Song Number</th>
-              <th>Song Title</th>
-              <th>Song Duration</th>
-            </tr>
             {
               this.state.album.songs.map( (song, index) => 
-                <tr key={index}>
+                <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
                   <td>{index+1}</td>
                   <td>{song.title}</td>
                   <td>{song.duration}</td>
